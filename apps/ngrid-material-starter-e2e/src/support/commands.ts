5471b812454a554c2b2ef5fd86f8e0edab4f80f4
Shlomi Assaf <shlomiasaf@gmail.com>
2020-03-15 10:02:01 +0200

    add nx and cypress + some testing for IVY & ViewEngine

---
+++ apps/ngrid-material-starter-e2e/src/support/commands.ts
@@ -0,0 +1,28 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+// eslint-disable-next-line @typescript-eslint/no-namespace
+
+import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
+
+declare global {
+  namespace Cypress {
+    interface Chainable<Subject> {
+      login(email: string, password: string): void;
+
+      matchImageSnapshot(snapShotName?: string, snapShotOptions?: any): void;
+      matchImageSnapshot(snapShotOptions: string): void;
+    }
+  }
+}
+
+// Cypress.env('updateSnapshots', true);
+addMatchImageSnapshotCommand({
+  customSnapshotsDir: 'snapshots'
+});
