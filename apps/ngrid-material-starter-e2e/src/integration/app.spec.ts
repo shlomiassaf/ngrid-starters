6867a4714fe7f9ee85e5d551bd8170af8048d9c2
Shlomi Assaf <shlomiasaf@gmail.com>
2020-03-15 10:55:04 +0200

    CI

--- apps/ngrid-material-starter-e2e/src/integration/app.spec.ts
+++ apps/ngrid-material-starter-e2e/src/integration/app.spec.ts
@@ -2,7 +2,8 @@ describe('ngrid-material-starter', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should load grid with data', () => {
+    cy.viewport(1200, 800);
     cy.wait(500);
-    cy.get('pbl-ngrid').matchImageSnapshot();
+    cy.matchImageSnapshot();
   });
 });
