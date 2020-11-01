1009b14357db2e27d62f447d485a4794607d41a6
Shlomi Assaf <shlomiasaf@gmail.com>
2020-11-01 23:07:59 +0200

    gh-action

---
+++ tools/build.gh-pages.sh
@@ -0,0 +1,2 @@
+node_modules/.bin/ng build ngrid-material-starter --prod --base-href ngrid-material-starter
+cp dist/apps/ngrid-material-starter/index.html dist/apps/ngrid-material-starter/404.html
--- tools/publish.gh-pages.sh
+++ tools/publish.gh-pages.sh
@@ -1,6 +1,3 @@
-node_modules/.bin/ng build ngrid-material-starter --prod --base-href ngrid-material-starter
-cp dist/apps/ngrid-material-starter/index.html dist/apps/ngrid-material-starter/404.html
-
 cd dist/apps/ngrid-material-starter
 
 git init
