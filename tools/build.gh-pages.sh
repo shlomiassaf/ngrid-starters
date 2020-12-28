46a38d28d25003ab1df4d396a10cf3603c1a8215
Shlomi Assaf <shlomiasaf@gmail.com>
2020-12-28 02:40:15 +0200

    fix gh-pages build

--- tools/build.gh-pages.sh
+++ tools/build.gh-pages.sh
@@ -1,5 +1,5 @@
-node_modules/.bin/ng build material --prod --base-href ngrid-starters/material --deploy-url ngrid-starters/material
+node_modules/.bin/ng build material --prod --base-href ngrid-starters/material/ --deploy-url ngrid-starters/material/
 cp dist/apps/ngrid-starters/material/index.html dist/apps/ngrid-starters/material/404.html
 
-node_modules/.bin/ng build bootstrap --prod --base-href ngrid-starters/bootstrap --deploy-url ngrid-starters/bootstrap
+node_modules/.bin/ng build bootstrap --prod --base-href ngrid-starters/bootstrap/ --deploy-url ngrid-starters/bootstrap/
 cp dist/apps/ngrid-starters/bootstrap/index.html dist/apps/ngrid-starters/bootstrap/404.html
